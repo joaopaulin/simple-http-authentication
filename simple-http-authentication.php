4d081f651228fc7ced0087852e4099ffb1164138
João Paulin <joao.paulin@mestreseo.com.br>
2015-07-08 17:58:29 -0300

    Added redirect to admin page.

--- simple-http-authentication.php
+++ simple-http-authentication.php
@@ -8,22 +8,25 @@ Version: 1.0
 Author URI: http://joaopaulin.com.br/
 */
 
+function simple_http_authentication_activation() {
+
+    wp_redirect( admin_url() );
+}
+
+register_activation_hook( __FILE__, 'simple_http_authentication_activation' );
+
 function simple_http_authentication() {
 
-	if( !is_admin() ) {
+    if ( is_wp_error( wp_authenticate( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] ) ) ) {
 
-	    if ( is_wp_error( wp_authenticate( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] ) ) ) {
+        header('WWW-Authenticate: Basic realm="' . wp_title( '-', false ) . '"');
+        header('HTTP/1.0 401 Unauthorized');
 
-	        header('WWW-Authenticate: Basic realm="' . wp_title( '-', false ) . '"');
-	        header('HTTP/1.0 401 Unauthorized');
+        echo __( 'You need to authenticate with a registered user on WordPress.', 'simple-http-authentication' );
 
-	        echo __( 'You need to authenticate with a registered user on WordPress.', 'simple-http-authentication' );
+        exit;
 
-	        exit;
-	        
-	    }
-
-	}
+    }
 
 }
 
