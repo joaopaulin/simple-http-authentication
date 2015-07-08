cbe26bf9a2db74e15ad0cb6fed30fbbc31732e7b
João Paulin <ppaulin23@hotmail.com.br>
2015-07-08 17:31:27 -0300

    Added restriction to admin area.

--- simple-http-authentication.php
+++ simple-http-authentication.php
@@ -10,15 +10,20 @@ Author URI: http://joaopaulin.com.br/
 
 function simple_http_authentication() {
 
-    if ( is_wp_error( wp_authenticate( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] ) ) ) {
+	if( !is_admin() ) {
 
-        header('WWW-Authenticate: Basic realm="' . wp_title( '-', false ) . '"');
-        header('HTTP/1.0 401 Unauthorized');
+	    if ( is_wp_error( wp_authenticate( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] ) ) ) {
 
-        echo __( 'You need to authenticate with a registered user on WordPress.', 'simple-http-authentication' );
+	        header('WWW-Authenticate: Basic realm="' . wp_title( '-', false ) . '"');
+	        header('HTTP/1.0 401 Unauthorized');
 
-        exit;
-    }
+	        echo __( 'You need to authenticate with a registered user on WordPress.', 'simple-http-authentication' );
+
+	        exit;
+	    }
+
+	}
+
 }
 
 add_action( 'init', 'simple_http_authentication' );
